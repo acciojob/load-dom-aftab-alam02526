e63667c26cc8276d3d390889de33ddfc7a3f27e1
aftab-alam02526 <aftab0447@gmail.com>
2025-09-27 17:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 //your JS code here. If required.
 document.addEventListener('DOMContentLoaded', function () {
- 
-  setTimeout(() => {
-   
-    const body = document.body;
 
-   
+  setTimeout(() => {
+    const body = document.body;
     const message = document.createElement('p');
     message.innerText = "DOM load success";
-
-   
     body.appendChild(message);
   }, 50); 
 });
