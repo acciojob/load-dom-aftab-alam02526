fa103cde92ceefbfb5af88179f116b429389f10b
aftab-alam02526 <aftab0447@gmail.com>
2025-09-27 17:11:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const body = document.querySelector('body');
+const newElement = document.createElement('p');
+
+newElement.innerText = "DOM load success";
+body.appendChild(newElement);
